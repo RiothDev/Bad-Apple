55f6dd8fc33e8547aeada81c54b1268fee72b675
Hazu <109932988+HazuDev@users.noreply.github.com>
2022-11-16 13:18:49 -0600

    Update Code.py

--- Code.py
+++ Code.py
@@ -28,6 +28,7 @@ def main():
     def loadMusic(song):
         pygame.mixer.init()
         pygame.mixer.music.load(f"Assets/{song}")
+        pygame.mixer.music.set_volume(1)
         pygame.mixer.music.play()
 
     def playVideo(totalFrames):
