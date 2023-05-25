9a61e9b28a1b21b4afa2ec31a70514db85c21579
Rioth <109932988+RiothDev@users.noreply.github.com>
2023-05-24 21:59:37 -0600

    Update Code.py

--- Code.py
+++ Code.py
@@ -3,15 +3,15 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 from PIL import Image
 
-asciiChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ","]
-asciiImages = []
+ASCII_CHARS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ","]
+ASCII_IMAGES = []
 
 class sizes:
     def __init__(self, width, height):
         self.Width = int(width)
         self.Height = int(height)
 
-frameCount = 1
+frame_count = 1
 
 cap = None
 
@@ -22,39 +22,39 @@ def main():
     sys.stdout.write("Start\n")
     sys.stdout.write("--------------\n")
 
-    def getSize():
-        screenSizes = [m for m in screeninfo.get_monitors()]
+    def get_size():
+        screen_sizes = [m for m in screeninfo.get_monitors()]
         monitor = 1
 
-        if len(screenSizes) > 1:
-            monitor = input("> Monitor (1 - {}) : ".format(len(screenSizes)))
+        if len(screen_sizes) > 1:
+            monitor = input("> Monitor (1 - {}) : ".format(len(screen_sizes)))
         
-        return math.floor(screenSizes[(int(monitor) - 1)].height / 5.5), math.floor(screenSizes[(int(monitor) - 1)].width / 10)
+        return math.floor(screen_sizes[(int(monitor) - 1)].height / 5.5), math.floor(screen_sizes[(int(monitor) - 1)].width / 10)
     
-    askSize = input("> Use screen sizes? (Y / N) : ")
+    ask_size = input("> Use screen sizes? (Y / N) : ")
 
-    if askSize.lower() == "y":
-        monitorHeight, monitorWidth = getSize()
+    if ask_size.lower() == "y":
+        monitor_height, monitor_width = get_size()
     else:
-        monitorHeight, monitorWidth = int(input("> Height (190) : ")), int(input("> Width (160) : "))
+        monitor_height, monitor_width = int(input("> Height (190) : ")), int(input("> Width (160) : "))
 
-    def loadMusic(song):
+    def play_music(song):
         pygame.mixer.init()
         pygame.mixer.music.load(f"Assets/{song}")
         pygame.mixer.music.set_volume(1)
         pygame.mixer.music.play()
 
-    def playVideo(totalFrames):
+    def play_video(totalFrames):
         print("Starting...")
         os.system("cls")
 
         timer = fpstimer.FPSTimer(30)
-        loadMusic("Song.mp3")
+        play_music("Song.mp3")
 
         for frame in range(0, totalFrames):
             try:
                 #sys.stdout.write("\n\n\n\n")
-                sys.stdout.write("\r" + asciiImages[frame])
+                sys.stdout.write("\r" + ASCII_IMAGES[frame])
                 sys.stdout.flush()
 
             except Exception as error:
@@ -62,68 +62,68 @@ def main():
 
             timer.sleep()
     
-    def loadFrames(totalFrames):
-        global frameCount
-        global asciiImages
+    def load_frames(totalFrames):
+        global frame_count
+        global ASCII_IMAGES
         global cap
 
         print("Loading frames...")
         r, frm = cap.read()
 
-        while r and frameCount <= totalFrames:
+        while r and frame_count <= totalFrames:
             r, frm = cap.read()
-            frameCount += 1
+            frame_count += 1
 
             try:
                 img = Image.fromarray(frm)
-                asciiImg = transform(img)
-                asciiImages.append(asciiImg)
+                ASCII_img = transform(img)
+                ASCII_IMAGES.append(ASCII_img)
 
             except Exception as error:
                 continue
         
         cap.release()
-        playVideo(frameCount)
+        play_video(frame_count)
     
-    def getFrames(path):
-        checkCap = cv2.VideoCapture(path)
-        totalFrames = int(checkCap.get(cv2.CAP_PROP_FRAME_COUNT))
+    def get_frames(path):
+        check_cap = cv2.VideoCapture(path)
+        totalFrames = int(check_cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-        checkCap.release()
+        check_cap.release()
 
         fpp = totalFrames - 1
-        loadFrames(fpp)
+        load_frames(fpp)
 
-    def loadVideo(vid):
+    def load_video(vid):
         global cap
         cap = cv2.VideoCapture(f"Assets/{vid}")
-        getFrames(f"Assets/{vid}")
+        get_frames(f"Assets/{vid}")
 
     def transform(img):
-        global mainSizes
+        global main_sizes
 
-        oldWidth, oldHeight = img.size
-        oldSizes = sizes(oldWidth, oldHeight)
+        old_width, old_height = img.size
+        old_sizes = sizes(old_width, old_height)
 
-        ratio = (oldSizes.Height / float(oldSizes.Width * 2.5))
+        ratio = (old_sizes.Height / float(old_sizes.Width * 2.5))
 
-        newSizes = sizes(monitorWidth, math.floor((ratio * monitorHeight)))
+        new_sizes = sizes(monitor_width, math.floor((ratio * monitor_height)))
         #print(math.floor((ratio * float(monitorSizes.height / 5.6))))
 
-        img = img.resize((newSizes.Width, newSizes.Height))
+        img = img.resize((new_sizes.Width, new_sizes.Height))
         img = img.convert("L")
 
         pixels = img.getdata()
 
-        newPixels = "".join([asciiChars[pixel // 25] for pixel in pixels])
-        pixelsLen = len(newPixels)
+        new_pixels = "".join([ASCII_CHARS[pixel // 25] for pixel in pixels])
+        pixels_len = len(new_pixels)
 
-        asciiImage = [newPixels[index:(index + newSizes.Width)] for index in range(0, pixelsLen, newSizes.Width)]
-        asciiImage = "\n".join(x for x in asciiImage)
+        ASCII_Image = [new_pixels[index:(index + new_sizes.Width)] for index in range(0, pixels_len, new_sizes.Width)]
+        ASCII_Image = "\n".join(x for x in ASCII_Image)
 
-        return asciiImage
+        return ASCII_Image
     
-    loadVideo("Video.mp4")
+    load_video("Video.mp4")
 
 if __name__ == "__main__":
     try:
@@ -132,3 +132,4 @@ if __name__ == "__main__":
         print(traceback.format_exc())
         input("")
         sys.exit(-1)
+
